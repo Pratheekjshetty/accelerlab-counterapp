0d14351e0312d51b577c3dce5cb9135c4a8d709b
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-07-24 11:14:26 +0530

    counter updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [count,setCount] = useState(0);
+  const handleIncrement =() =>(
+    setCount(count+1)
+  )
+  const handleDecrement =()=>(
+    setCount(count-1)
+  )
   return (
-    <div className="App">
+    <div className="container">
+      <div>
+        <div className="counter">
+          <p>{count}</p>
+        </div>
+        <div className="button">
+            <button onClick={handleIncrement} className="Increment">Increment</button>
+            <button onClick={handleDecrement} className="Decrement">Decrement</button>
+        </div>
+      </div>
     </div>
   );
 }
